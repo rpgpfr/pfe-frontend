4cda2fd1d00fdcf44cc1844461bfe0ace3606164
Philippe Plaïa <philippeplaia@gmail.com>
2025-02-21 17:12:09 +0100

    feat: new component header for the homepage

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import styles from '@/app/components/Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.Titre}>
+            <Link href="#accueil" className={styles.accueil}>
+                    PROJECT RPG
+                </Link>           
+            </div>
+            <nav className={styles.nav}>
+                <Link href="#seconnecter" className={styles.link}>
+                    Se Connecter
+                </Link>
+                <Link href="#sinscrire" className={styles["btn-primary"]}>
+                    S'inscrire
+                </Link>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
