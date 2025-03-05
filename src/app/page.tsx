b9034ca95552cadafee1d35aafcf41d506ca5dfb
Philippe Plaïa <philippeplaia@gmail.com>
2025-03-05 13:56:11 +0100

    feat: add Aladin font for the homepage and add some changes to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/block/header/Header";
 import Footer from "@/block/footer/Footer";
+import Hero from "@/block/hero/Hero";
 
 const Home = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <div className="flex-grow">
+                <Hero />
             </div>
             <Footer />
         </div>
