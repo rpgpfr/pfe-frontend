d2889cf27d7eb44195aa9e3c05b5d14b3057228f
Philippe Plaïa <philippeplaia@gmail.com>
2025-03-10 23:22:01 +0100

    feat: add new section for the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/block/header/Header";
 import Footer from "@/block/footer/Footer";
 import Hero from "@/block/hero/Hero";
 import Presentation from "@/block/presentation/presentation";
+import CampaignCreator from "@/block/campaign-creator/campaign-creator";
 
 const Home = () => {
     return (
@@ -12,6 +13,9 @@ const Home = () => {
             </div>
             <div>
                 <Presentation />
+            </div>
+            <div>
+                <CampaignCreator />
             </div>
             <Footer />
         </div>
