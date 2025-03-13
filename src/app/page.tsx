6d16923ee51690bae85e53eef50b88382d67ec48
Philippe Plaïa <philippeplaia@gmail.com>
2025-03-13 22:56:10 +0100

    feat: add all bloc section for the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import Header from "@/block/header/Header";
 import Footer from "@/block/footer/Footer";
 import Hero from "@/block/hero/Hero";
 import Presentation from "@/block/presentation/presentation";
-import CampaignCreator from "@/block/campaign-creator/campaign-creator";
+import SectionOne from "@/block/section_one/section_one";
+import SectionTwo from "@/block/section_two/section_two";
+import SectionThree from "@/block/section_three/section_three";
 
 const Home = () => {
     return (
@@ -15,6 +17,13 @@ const Home = () => {
                 <Presentation />
             </div>
             <div>
+                <SectionOne />
+            </div>
+            <div>
+                <SectionTwo />
+            </div>
+            <div>
+                <SectionThree />
             </div>
             <Footer />
         </div>
