73495293f6829cb0b4013b5eeeaf6bdadaf2e88a
Alvin Hamaïde <alvin98mc@gmail.com>
2025-04-28 00:42:51 +0200

    feat: created separator and goal input components

---
+++ src/components/ui/Separator/Separator.tsx
@@ -0,0 +1,24 @@
+import {FC, ReactNode} from "react";
+import {clsx} from "clsx";
+
+import styles from "./Separator.module.css";
+
+interface SeparatorProps {
+    children?: ReactNode;
+}
+
+const Separator: FC<SeparatorProps> = ({children}) => {
+
+    const separatorClassNames = clsx(styles.separator, children ? "h-fit": "h-px");
+
+    return (
+        <div className={separatorClassNames}>
+            <span></span>
+            {children}
+            <span></span>
+        </div>
+    );
+
+};
+
+export default Separator;
