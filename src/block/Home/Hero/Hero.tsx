779b18ef52130f9b2a674609e99fbe954e82012f
Alvin Hamaïde <alvin98mc@gmail.com>
2025-05-21 10:00:19 +0200

    feature/images (#12)
    * fix(images): fixed middleware regex and added images for login and signup
    
    * feat(images): added images for hero, banners, campaigns and characters

--- src/block/Home/Hero/Hero.tsx
+++ src/block/Home/Hero/Hero.tsx
@@ -1,11 +1,20 @@
 import {Button} from "@/components/ui";
-import {aladin} from "@/lib/utils";
+import {aladin, randomInt} from "@/lib/utils";
 
 import styles from './Hero.module.css';
+import Image from "next/image";
 
 const Hero = () => {
     return (
         <div className={styles.hero}>
+            <div className={styles.backgroundWrapper}>
+                <Image
+                    src={`/images/hero-${randomInt(1, 4)}.png`}
+                    alt=""
+                    fill
+                />
+            </div>
+
             <p className={`${styles.title} ${aladin.className}`}>Projet RPG</p>
             <Button variant="primary" href="/campaigns">Créer une nouvelle campagne</Button>
         </div>
