ee9b957af9a12f450ea053efc76efd696f081659
Tom Engélibert <tomengelibert@MacBook-Pro-de-Tom.local>
2025-05-27 22:39:09 +0200

    feat(comingsoon.tsx): ajout du composant comingSoon

--- src/components/ComingSoon/ComingSoon.tsx
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -1,8 +1,11 @@
 import styles from './ComingSoon.module.css';
+import FlagIcon from '/public/images/icons/flag.svg';
+import Image from "next/image";
 const ComingSoon = () => {
     return (
-        <div className={styles.comingSoon}>
+        <div className={styles.container}>
             <h2 className={styles.title}>Coming Soon</h2>
+            <Image className={styles.image} src={FlagIcon} alt="Icone de drapeau" width={20} height={20}/>
             <p className={styles.text}>Cette fonctionnalité est en cours de développement.</p>
         </div>
     );
