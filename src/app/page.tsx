5525188b81b9cdeef34692cb01705eb4564699a0
Tom Engélibert <tomengelibert@MacBook-Pro-de-Tom.local>
2025-05-28 20:46:05 +0200

    refactor(page.tsx): suppression du composant upcoming pour le test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import {CampaignDemo, Hero, Presentation, UsecaseDemo, WorldDemo} from "@/block/Home";
-import {ComingSoon} from "@/components";
 
 const Home = () => {
 
     return (
         <main className="flex flex-col min-h-screen">
             <Hero/>
-            <ComingSoon/>
             <Presentation/>
             <div className="mx-[8%]">
             <CampaignDemo/>
